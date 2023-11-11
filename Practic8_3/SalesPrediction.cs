de2cbf0cd448e8a92a0416967db7da6377585fca
Kambarov.Dilshat <issyk1999iphone@gmail.com>
2023-11-11 23:33:45 +0600

    Добавьте файлы проекта.

---
+++ Practic8_3/SalesPrediction.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul8
+{
+    //практика 8_3
+    using System;
+    using System.Linq;
+
+    class SalesPrediction
+    {
+        static void Main()
+        {
+            int[] time = { 1, 2, 3, 4, 5 };
+            double[] sales = { 200, 240, 230, 280, 270 }; // Пример данных продаж
+
+            double meanTime = time.Average();
+            double meanSales = sales.Average();
+
+            double sumTimeSales = time.Zip(sales, (t, s) => t * s).Sum();
+            double sumTimeSquared = time.Select(t => t * t).Sum();
+
+            int N = time.Length;
+
+            double a = (N * sumTimeSales - time.Sum() * sales.Sum()) / (N * sumTimeSquared - Math.Pow(time.Sum(), 2));
+            double b = meanSales - a * meanTime;
+
+            for (int i = 6; i <= 8; i++)
+            {
+                double forecast = a * i + b;
+                Console.WriteLine($"Прогноз продаж для месяца {i}: {forecast}");
+            }
+        }
+    }
+
+}
---
+++ Practic_8_2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practic_8_2
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Supermarket
+    {
+        static void Main()
+        {
+            var products = new Dictionary<string, double>
+        {
+            {"Молоко", 50.0},
+            {"Хлеб", 30.0},
+            {"Сыр", 200.0}
+        };
+
+            double totalSum = 0;
+            foreach (var product in products)
+            {
+                totalSum += product.Value;
+            }
+
+            TimeSpan startTime = new TimeSpan(8, 0, 0);
+            TimeSpan endTime = new TimeSpan(12, 0, 0);
+            TimeSpan currentTime = DateTime.Now.TimeOfDay;
+
+            if (currentTime > startTime && currentTime < endTime)
+            {
+                totalSum *= 0.95; // 5% скидка
+            }
+
+            Console.WriteLine($"Общая сумма: {totalSum}");
+        }
+    }
+
+}
